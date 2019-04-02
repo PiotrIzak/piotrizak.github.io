509cb51f7047157ac7394c3e4b59af71397bfbe0
Piotr Izak <pizak@objectivity.co.uk>
2019-04-02 10:19:53 +0200

    fetch update

--- sw.js
+++ sw.js
@@ -5,5 +5,10 @@ self.addEventListener('activate', function(ev) {
   console.log("Zdarzenie wywoływane po aktualizacji pliku Service Workera");
 });
 self.addEventListener('fetch', function(ev) {
-  console.log("Zdarzenie wywoływane podczas próby pobrania zasobu");
+  if (ev.request.url.endsWith('.worker')) {
+    ev.respondWith(new Response('<strong>Ten URL istnieje!</strong>',
+    {headers:
+     {"Content-type":"text/html"}
+   }));
+  }
 });
